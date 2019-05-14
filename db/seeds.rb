a4c7f6dd5c3d087e5f5d81946e8ec46b6f78ae0d
danizaghian <dani.zaghian@gmail.com>
2019-05-14 11:50:27 -0700

    adds seed dump gem and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
-contact = Contact.create(first_name: "Benjamin", last_name: "Kamp", email: "benjamin@gmail.com", phone_number: "830-1847")
-contact = Contact.create(first_name: "Brian", last_name: "Chinn", email: "brian@gmail.com", phone_number: "291-5873")
-	
+Contact.create!([
+  {first_name: "Paul", last_name: "Ollerton", email: "paul@gmail.com", phone_number: "473-9285", middle_name: "Brooke", bio: nil, active: true},
+  {first_name: "Sarah", last_name: "Heimerdinger", email: "sarah@gmail.com", phone_number: "249-1947", middle_name: "Brooke", bio: nil, active: true},
+  {first_name: "Isreal", last_name: "Hass", email: "isreal@gmail.com", phone_number: "218-0823", middle_name: "Brooke", bio: nil, active: true},
+  {first_name: "Benjamin", last_name: "Kamp", email: "benjamin@gmail.com", phone_number: "830-1847", middle_name: "Brooke", bio: nil, active: true},
+  {first_name: "Brian", last_name: "Chinn", email: "brian@gmail.com", phone_number: "291-5873", middle_name: "Brooke", bio: nil, active: true},
+  {first_name: "Benjamin", last_name: "Kamp", email: "benjamin@gmail.com", phone_number: "830-1847", middle_name: "Brooke", bio: nil, active: true},
+  {first_name: "Brian", last_name: "Chinn", email: "brian@gmail.com", phone_number: "291-5873", middle_name: "Brooke", bio: nil, active: true},
+  {first_name: "Janine", last_name: "Lazarova", email: "janine@gmail.com", phone_number: "392-5891", middle_name: "Brooke", bio: "Loves pina coladas and getting caught in the rain.", active: true},
+  {first_name: "Kaitlyn", last_name: "Higgins", email: "kaitlyn@gmail.com", phone_number: "853-2853", middle_name: "Brooke", bio: "Other people can't spell her last name.", active: true}
+])
